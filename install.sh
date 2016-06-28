10b5204449b946fb740e707805039af5db796db8
Len <a78965@gmail.com>
2016-06-28 19:44:36 +0800

    pass all yes verbose messages

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # install apache
-sudo apt-get update && sudo apt-get install apache2
-
-# install mysql server
-sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo apt-get update && sudo apt-get install -y apache2
 
 # install php5 and modules
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+
+# install mysql server
+sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
 
 # install phpmyadmin
-sudo apt-get install phpmyadmin
+sudo apt-get install -y phpmyadmin
